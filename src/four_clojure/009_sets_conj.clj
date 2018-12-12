9e458582cf87b6b365620bc46f8d41ca45cdad17
John Stevenson <john@jr0cket.co.uk>
2018-12-12 19:56:26 +0000

    009: Sets with conj
    A very simple challenge using conj to add values to a set.

---
+++ src/four_clojure/009_sets_conj.clj
@@ -0,0 +1,45 @@
+(ns four-clojure.009-sets-conj)
+
+;; #9 Sets: conj
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Difficulty:	Elementary
+;; Topics:
+
+;; When operating on a set, the conj function returns a new set with one or more keys "added".
+
+;; (= #{1 2 3 4} (conj #{1 4 3} __))
+
+
+;; Deconstruct the problem
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; A simple challenge to show how `conj` works with sets.
+
+;; REPL experiments
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; As discussed in challenge #8, sets do not really care about order
+;; it is only the values within the set that is important
+
+;; So if the result we want is `#{1 2 3 4}`
+;; then we just need to conjoin the missing number
+
+(conj #{1 4 3} 2)
+;; => #{1 4 3 2}
+
+
+;; Answers summary
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Most readable answer
+2
+
+;; Superfluous answers
+
+;; As conj can take multiple values you could add any of the values already in the set,
+;; so long as you are adding 2 also.
+;; Extra values will just be stripped out.
+
+;; 1 2 3 4
+;; 1 2 3 4 1 2 3 4 1 2 3 4
