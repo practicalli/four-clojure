abce7eea356cba5594eecd51b5b67ecfd1bb5994
John Stevenson <john@jr0cket.co.uk>
2019-10-19 17:54:03 +0100

    064 Intro to Reduce
    An elementry level introduction to how the reduce function works.  Used as a
    recap for challenge 63

---
+++ src/four_clojure/064_intro_to_reduce.clj
@@ -0,0 +1,64 @@
+(ns four-clojure.064-intro-to-reduce)
+
+;; #64 Intro to Reduce
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Difficulty:	Elementary
+;; Topics:	seqs
+
+
+;; Reduce takes a 2 argument function and an optional starting value.
+;; It then applies the function to the first 2 items in the sequence
+;; (or the starting value and the first element of the sequence).
+;; In the next iteration the function will be called on the previous return value
+;; and the next item from the sequence,
+;; thus reducing the entire collection to one value.
+
+;; Don't worry, it's not as complicated as it sounds.
+
+
+;; (= 15 (reduce __ [1 2 3 4 5]))
+;; (=  0 (reduce __ []))
+;; (=  6 (reduce __ 1 [2 3]))
+
+
+;; Deconstruct the problem
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; With some simple mathematics, we can solve the 4Clojure tests fairly easily.
+
+;; reduce calls a function on each of the values in the sequence.
+
+;; so if we want the result `15` from the numbers `[1 2 3 4 5]`
+;; then we can use the `+` function with reduce
+
+
+(= 15 (reduce + [1 2 3 4 5]))
+
+;; We can see the single value that this produces
+(reduce + [1 2 3 4 5])
+
+;; and we can see how its working it all out using `reductions`
+
+(reductions + [1 2 3 4 5])
+
+
+(reduce + [1 2 3 4 5])
+
+;; translates to:
+
+(+ (+ (+ (+ 1 2) 3) 4) 5)
+
+
+;; References
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://clojuredocs.org/clojure.core/reduce
+;; https://github.com/clojure/clojure/blob/clojure-1.9.0/src/clj/clojure/core.clj#L6730
+
+
+
+
+;; Answers summary
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; +
