4cb9aaeab442469aea0c3220f3cea7a9b1f98308
Practicalli Engineering <engineering@practical.li>
2025-04-12 15:06:38 +0100

    dev: deps.edn project config, drop leiningen

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject four-clojure "0.1.0-SNAPSHOT"
-  :description "Discussions on the solutions to the 4Clojure.com challenges"
-  :url "https://github.com/jr0cket/four-clojure"
-  :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
-            :url "https://creativecommons.org"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
